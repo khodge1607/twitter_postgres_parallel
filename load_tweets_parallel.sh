28046f26a20982cc04ad43de42535b3aa0d3ed1a
khodge1607 <kgraceh.04@gmail.com>
2026-04-14 16:58:16 -0700

    fixed noramlized permission issue?

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -12,7 +12,7 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
-time echo "$files" | parallel ./load_normalized.sh
+time echo "$files" | parallel bash ./load_normalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
